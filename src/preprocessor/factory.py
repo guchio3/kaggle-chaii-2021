11bc2c588e2ca37295754924cdec6a8978f62225
guchio3 <astt.hwhw@gmail.com>
2021-11-10 01:26:57 +0900

    temp

--- src/preprocessor/factory.py
+++ src/preprocessor/factory.py
@@ -10,6 +10,7 @@ from src.preprocessor.preprocessor import (BaselineKernelPreprocessorV1,
                                            BaselineKernelPreprocessorV6,
                                            BaselineKernelPreprocessorV7,
                                            BaselineKernelPreprocessorV8,
+                                           BaselineKernelPreprocessorV9,
                                            Preprocessor)
 from src.repository.data_repository import DataRepository
 
@@ -146,6 +147,20 @@ class PreprocessorFactory(Factory[Preprocessor]):
             )
         elif self.preprocessor_type == "baseline_kernel_v8":
             preprocessor = BaselineKernelPreprocessorV8(
+                tokenizer=tokenizer,
+                data_repository=data_repository,
+                max_length=self.max_length,
+                pad_on_right=self.pad_on_right,
+                stride=self.stride,
+                split=self.split,
+                lstrip=self.lstrip,
+                use_language_as_question=self.use_language_as_question,
+                add_overflowing_batch_id=self.add_overflowing_batch_id,
+                debug=self.debug,
+                logger=self.logger,
+            )
+        elif self.preprocessor_type == "baseline_kernel_v9":
+            preprocessor = BaselineKernelPreprocessorV9(
                 tokenizer=tokenizer,
                 data_repository=data_repository,
                 max_length=self.max_length,
--- src/preprocessor/preprocessor.py
+++ src/preprocessor/preprocessor.py
@@ -595,3 +595,30 @@ class BaselineKernelPreprocessorV8(BaselineKernelPreprocessorV6):
         else:
             start_char_index = int(row["answer_start"])
         return start_char_index
+
+
+class BaselineKernelPreprocessorV9(BaselineKernelPreprocessorV6):
+    def _pre_postprocess(
+        self, preprocessed_results: List[Tuple[int, int, Series, bool]]
+    ) -> List[Tuple[int, int, Series, bool]]:
+        max_successed_duplicated_elems_num_with = 0
+        for i, j, row, is_successed in preprocessed_results:
+            if row["duplicated_elems_num_with"] == -1:
+                raise Exception("duplicated_elems_num_with should not be -1.")
+            if is_successed:
+                max_successed_duplicated_elems_num_with = max(
+                    max_successed_duplicated_elems_num_with,
+                    row["duplicated_elems_num_with"],
+                )
+        res_preprocessed_results = []
+        for i, j, row, is_successed in preprocessed_results:
+            row[
+                "max_successed_duplicated_elems_num_with"
+            ] = max_successed_duplicated_elems_num_with
+            if (
+                is_successed
+                and row["duplicated_elems_num_with"]
+                == max_successed_duplicated_elems_num_with
+            ):
+                res_preprocessed_results.append((i, j, row, is_successed))
+        return res_preprocessed_results
